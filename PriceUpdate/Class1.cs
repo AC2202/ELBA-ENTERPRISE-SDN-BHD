cd6f0f9064332b583f96e495992dbc65bf013502
centr <centr@SD35>
2026-01-19 16:23:42 +0800

    Add project files.

---
+++ PriceUpdate/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoCount;
+using AutoCount.Scripting;
+using AutoCount.PlugIn;
+
+namespace PriceUpdate
+{
+    public class PlugInit : AutoCount.PlugIn.BasePlugIn
+    {
+        public PlugInit() : base(new Guid("{46771C32-841E-4901-9AB0-C668D1062BEC}"), "Selling Price in Stock Transfer", "2.2.0", "engkeat.cheow@softwaredepot.com.my")
+        {
+            SetMinimumAccountingVersionRequired("2.2.22.30");
+            SetIsFreeLicense(false);
+            SetDevExpressComponentVersionRequired("22.2.7");
+            SetCopyright("Software Depot Sdn Bhd");
+        }
+
+        public override bool BeforeLoad(BeforeLoadArgs e)
+        {
+            AutoCount.Scripting.ScriptManager scriptManager = AutoCount.Scripting.ScriptManager.GetOrCreate(e.DBSetting);
+            scriptManager.RegisterByType("XFER", typeof(StockTransferScript));
+            return base.BeforeLoad(e);
+        }
+
+        public override void GetLicenseStatus(LicenseStatusArgs e)
+        {
+            e.LicenseStatus = LicenseStatus.Custom;
+            e.CustomLicenseStatus = $"License is registered to {e.CompanyProfile.CompanyName}";
+        }
+
+
+
+    }
+}
---
+++ PriceUpdate/StockTransferScript.cs
@@ -0,0 +1,141 @@
+﻿namespace PriceUpdate
+{
+    internal class StockTransferScript
+    {
+        public void OnMasterColumnChanged(AutoCount.Stock.StockTransfer.StockTransferMasterColumnChangedEventArgs e)
+        {
+            if (e.ChangedColumnName == "UDF_CUST")
+            {
+                for (int i = 0; i < e.MasterRecord.DetailCount; i++)
+                {
+                    string transferCust = e.MasterRecord.UDF["CUST"].ToString();
+
+                    object debtorType = e.DBSetting.ExecuteScalar("SELECT DebtorType FROM Debtor Where AccNo=?",
+                        transferCust);
+                    string sDebtorType = debtorType.ToString();
+                    string item = e.MasterRecord.GetDetailRecord(i).ItemCode.ToString();
+
+
+                    if (sDebtorType == "CSGN-EM")
+                    {
+                        object price = e.DBSetting.ExecuteScalar("SELECT UDF_CSGNEM FROM Item Where ItemCode=?",
+                            item);
+                        e.MasterRecord.GetDetailRecord(i).UnitCost = AutoCount.Converter.ToDecimal(price);
+
+                    }
+
+                    else if (sDebtorType == "CSGN-PEN")
+                    {
+                        object price = e.DBSetting.ExecuteScalar("SELECT UDF_CSGNPEN FROM Item Where ItemCode=?",
+                            item);
+                        e.MasterRecord.GetDetailRecord(i).UnitCost = AutoCount.Converter.ToDecimal(price);
+                    }
+
+                    else if (sDebtorType == "CSGN-WE")
+                    {
+                        object price = e.DBSetting.ExecuteScalar("SELECT UDF_CSGNWE FROM Item Where ItemCode=?",
+                            item);
+                        e.MasterRecord.GetDetailRecord(i).UnitCost = AutoCount.Converter.ToDecimal(price);
+                    }
+
+                    else if (sDebtorType == "OURGT-FP")
+                    {
+                        object price = e.DBSetting.ExecuteScalar("SELECT UDF_OURGTFP FROM Item Where ItemCode=?",
+                            item);
+                        e.MasterRecord.GetDetailRecord(i).UnitCost = AutoCount.Converter.ToDecimal(price);
+                    }
+
+                    else if (sDebtorType == "OURGT-PEN")
+                    {
+                        object price = e.DBSetting.ExecuteScalar("SELECT UDF_OURGTPEN FROM Item Where ItemCode=?",
+                            item);
+                        e.MasterRecord.GetDetailRecord(i).UnitCost = AutoCount.Converter.ToDecimal(price);
+                    }
+
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+
+        public void OnDetailColumnChanged(AutoCount.Stock.StockTransfer.StockTransferDetailColumnChangedEventArgs e)
+        {
+
+            if (e.ChangedColumnName == "ItemCode" || e.ChangedColumnName == "Qty")
+            {
+                if (e.MasterRecord.UDF["CUST"].ToString() == "")
+                {
+                    AutoCount.AppMessage.ShowMessage("Please select Customer at UDF Tab first");
+                }                
+                string transferCust = e.MasterRecord.UDF["CUST"].ToString();
+                object debtorType = e.DBSetting.ExecuteScalar("SELECT DebtorType FROM Debtor Where AccNo=?",
+                    transferCust);
+                string sDebtorType = debtorType.ToString();
+                string item = e.CurrentDetailRecord.ItemCode.ToString();
+
+                if (sDebtorType == "CSGN-EM")
+                {
+                    //AutoCount.Application.Properties.DBSetting.ExecuteScalar("");
+                    object price = e.DBSetting.ExecuteScalar("SELECT UDF_CSGNEM FROM Item Where ItemCode=?",
+                        item);
+                    e.CurrentDetailRecord.UnitCost = AutoCount.Converter.ToDecimal(price);
+                }
+
+                else if (sDebtorType == "CSGN-PEN")
+                {
+                    object price = e.DBSetting.ExecuteScalar("SELECT UDF_CSGNPEN FROM Item Where ItemCode=?",
+                        item);
+                    e.CurrentDetailRecord.UnitCost = AutoCount.Converter.ToDecimal(price);
+                }
+
+                else if (sDebtorType == "CSGN-WE")
+                {
+                    object price = e.DBSetting.ExecuteScalar("SELECT UDF_CSGNWE FROM Item Where ItemCode=?",
+                        item);
+                    e.CurrentDetailRecord.UnitCost = AutoCount.Converter.ToDecimal(price);
+                }
+
+                else if (sDebtorType == "OURGT-FP")
+                {
+                    object price = e.DBSetting.ExecuteScalar("SELECT UDF_OURGTFP FROM Item Where ItemCode=?",
+                        item);
+                    e.CurrentDetailRecord.UnitCost = AutoCount.Converter.ToDecimal(price);
+                }
+
+                else if (sDebtorType == "OURGT-PEN")
+                {
+                    object price = e.DBSetting.ExecuteScalar("SELECT UDF_OURGTPEN FROM Item Where ItemCode=?",
+                        item);
+                    e.CurrentDetailRecord.UnitCost = AutoCount.Converter.ToDecimal(price);
+                }
+
+                else
+                {
+                    return;
+                }
+
+            }
+
+        }
+
+
+        public void BeforeSave(AutoCount.Stock.StockTransfer.StockTransferBeforeSaveEventArgs e)
+        {
+            if (e.MasterRecord.UDF["CUST"] == null)
+            {
+                e.AbortSave();
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+    }
